0f77b8c308e62ab3f6e0aeb2f4ad02c5cf84a17a
dontito <francistito07@gmail.com>
2017-08-29 16:08:38 +0300

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,72 +40,58 @@
         </a>
     </section>
 
-<div class="container">
+<div class="container-fluid">
    <section>
         <div class="row">
             <div class="col-lg-12">
                 <h2 class="page-header">Portfolio Heading</h2>
             </div>
           </div>
+
              <div class="row">
-<div class="col-md-4 col-sm-12">
+              <div class="col-md-10">
+          <div class="row">
+<div class="col-md-4">
     <div class="card">
         <img src="image/infosec-training.png" alt="Avatar" style="height:200px;" >
-        <div class="container">
-        <h4><b>Training</b></h4> 
-         <a href="/training" class="btn btn-danger">Information Security tools </a>
+          <h4 class="card-title" style="margin-left:30px;">Training And Events</h4>
+         <a href="/training" class="btn btn-danger">Security Training</a>
     </div>
-  </div>
  </div>
-       <div class="col-md-4 col-sm-12">
+       <div class="col-md-4">
     <div class="card">
         <img src="image/firewall-antivirus-antispyware-signpost-showing-internet-and-computer-secur_M1kqMXPu.jpg" alt="Avatar"  style="height:200px;">
-        <div class="container">
-        <h4><b>Tools</b></h4> 
-         <a href="/tools" class="btn btn-danger">Information Security tools </a>
-    </div>
+          <h4 class="card-title" style="margin-left:30px;">Security Service</h4>
+         <a href="/service" class="btn btn-danger">Security Services</a>
   </div>
  </div>
-       <div class="col-md-4 col-sm-12">
-    <div class="card">
-        <img src="image/iss_final1_white.jpg" alt="Avatar" >
-        <div class="container">
-        <h4><b>Services</b></h4> 
-         <a href="/service" class="btn btn-danger">Manage Security services</a>
-    </div>
+
   </div>
- </div>
-</div>
-<div class="row">
-    <div class="col-md-4 col-sm-12">
+
+<div class="row"  style="margin-top:20px;">
+    <div class="col-md-4">
     <div class="card">
         <img src="image/010713_1030_EHRPHISecur1.png" alt="Avatar" style="heigth:200px;">
-        <div class="container">
-        <h4><b>Assessment</b></h4> 
-     <a href="securityassessment.html" class="btn btn-danger">Security assessment and penetration</a>
-
-    </div>
+          <h4 class="card-title" style="margin-left:30px;">Security Tools</h4>
+     <a href="/tools" class="btn btn-danger">Security Tools</a>
   </div>
  </div>
-    <div class="col-md-4 col-sm-12">
-    <div class="card">
+    <div class="col-md-4">
+    <div class="card" sytle="padding:10px;">
         <img src="image/Risk-Manager-Risk-Management-Enterprise-RIsk-Management-300x200.jpg" alt="Avatar" >
-        <div class="container">
-        <h4><b>Risk Managements</b></h4> 
-  <a href="securityassessment.html" class="btn btn-danger">Risk management</a>
-    </div>
+          <h4 class="card-title" style="margin-left:30px;">Security Standards</h4>
+  <a href="/securitystandards" class="btn btn-danger">Security Standards</a>
   </div>
- </div>
-    <div class="col-md-4 col-sm-12">
-    <div class="card">
-        <img src="image/Risk-Manager-Risk-Management-Enterprise-RIsk-Management-300x200.jpg" alt="Avatar" >
-        <div class="container">
-        <h4><b>Information Security standard</b></h4> 
-  <a href="/securitystandards" class="btn btn-danger">Standards</a>
-    </div>
-  </div>
- </div>
+ </div> 
 </div>
+</div>
+
+<div class="col-md-2">
+<h1>news</h1>
+
+</div>
+
+    
     </section>
 
 </div>
